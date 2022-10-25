2b213eea822b01c2f0e52d78a3e1a4d36996b45d
ShAkEeB <shakeeb@ShAkEeBs-MacBook-Pro.local>
2022-10-25 22:05:25 +0600

    modify some responsive class

--- src/Components/Courses.jsx
+++ src/Components/Courses.jsx
@@ -10,8 +10,8 @@ const Courses = () => {
             .then(data => setCourses(data))
     }, [])
     return (
-        <div className='flex justify-center mr-12 '>
-            <div className=" w-1/3 mt-12 ml-5 mr-3">
+        <div className='md:flex justify-center md:mr-12 '>
+            <div className="w-full md:w-1/3 mt-12 md:ml-5 md:mr-3 text-center md:text-left">
                 {
                     courses?.map(re => <CourseNav
                         key={re.id}
@@ -19,7 +19,7 @@ const Courses = () => {
                     />)
                 }
             </div>
-            <div className="grid grid-cols-3 gap-5 py-12 ">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 py-12 px-5 md:px-0">
                 {
                     courses?.map(re => <CourseImg
                         key={re.id}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,29 +4,24 @@ import { authUser } from '../Context/UserContext';
 
 
 const Navbar = () => {
-
     const { user, logutOut } = useContext(authUser)
     return (
-        <div className="navbar bg-base-100 px-12 ">
+        <div className="navbar bg-base-100 md:px-12 ">
             <div className="flex-1">
                 <Link to='/' className="btn btn-ghost normal-case text-xl">Educapedia</Link>
             </div>
-            <div className="flex-none mr-20">
+            <div className=" md:mr-20 mx-auto">
                 <ul className="menu menu-horizontal p-0">
                     <li><NavLink to='/courses'>Courses</NavLink></li>
-                    <li><NavLink to='/faq'>FAQ</NavLink></li>
                     <li><NavLink to='/blog'>Blog</NavLink></li>
-                </ul>
-                <div className="form-control">
                     <label className="label cursor-pointer">
                         <input type="checkbox" className="toggle" />
                     </label>
-                </div>
-
+                </ul>
             </div>
             <div className="flex-none">
                 {user ?
-                    <div className="tooltip tooltip-left" data-tip={user?.displayName}>
+                    <div className="tooltip tooltip-left " data-tip={user?.displayName}>
                         <div className="dropdown dropdown-end">
                             <label tabIndex={0} className="btn btn-ghost btn-circle avatar ">
                                 <div className="w-10 rounded-full">
