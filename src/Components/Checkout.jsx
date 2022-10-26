b72b22c58c802c1f20c5ac4bd0b7e4a971c5f804
ShAkEeB <shakeeb@ShAkEeBs-MacBook-Pro.local>
2022-10-26 23:37:15 +0600

    add lotti loading

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,12 +1,18 @@
+import Lottie from 'lottie-react';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import thankAmimation from '../assets/thanks.json';
 
 const Checkout = () => {
     const data = useLoaderData()
-    const { name } = data;
+    const { name, id } = data;
     return (
-        <div>
-            {name}
+        <div className='text-center mt-10'>
+            <div className="w-1/5 mx-auto">
+                <Lottie animationData={thankAmimation} loop={false} />
+            </div>
+            <p> you are selected ID number -{id}</p>
+            <p className=' text-2xl'>{name}</p>
         </div>
     );
 };
